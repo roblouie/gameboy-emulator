da4735860989f8f129ff96e54ded53b668757ab1
mostofthings <62969747+mostofthings@users.noreply.github.com>
2021-03-19 00:15:09 -0500

    bit operations
    • added operations from bit-operations.ts to operations.ts

--- src/instruction-set/operations.ts
+++ src/instruction-set/operations.ts
@@ -2,10 +2,12 @@ import { inputOutputInstructions } from "./input-output/input-output";
 import { arithmeticAndLogicalOperations } from "./arithmetic-and-logical/arithmetic-and-logical-operations";
 import { rotateShiftOperations } from "./rotate-shift/rotate-shift-operations";
 import { jumpOperations } from "./jump/jump-operations";
+import { bitOperations } from "./bit/bit-operations";
 
 export const operations = [
   ...inputOutputInstructions,
   ...arithmeticAndLogicalOperations,
   ...rotateShiftOperations,
   ...jumpOperations,
+  ...bitOperations,
 ]
