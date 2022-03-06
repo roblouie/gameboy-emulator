a96fe19982a541e3855fae8219f55bd29f7029f4
Robert Louie <rob.j.louie@gmail.com>
2022-03-06 13:57:33 -0600

    Fix bug introduced during code cleanup.

---
+++ emulator/src/cpu/operations/cb-operations/cb-operation.ts
@@ -0,0 +1,10 @@
+
+export const cbOperation = {
+  instruction: '',
+  byteDefinition: 0xcb,
+  cycleTime: 0,
+  byteLength: 0,
+  execute() {
+    // No Operation, a sub operation will be looked up for this op.
+  }
+}
